6de159a1c3668d6322e7b5e7422595e71bf4cba2
Pascal <pascal.knecht@hotmail.ch>
2020-04-04 23:14:57 +0200

    Section 89 - fragments

--- src/App.js
+++ src/App.js
@@ -56,7 +56,7 @@ const App = () => {
 };
 
 const Search = ({search, onSearch}) => (
-    <div>
+    <>
         <label htmlFor="search">Search: </label>
         <input
             id="search"
@@ -64,7 +64,7 @@ const Search = ({search, onSearch}) => (
             value={search}
             onChange={onSearch}
         />
-    </div>
+    </>
 );
 
 const List = ({list}) =>
