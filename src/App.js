c1143c17cca672d18ffc51846f8dcb9247ed453c
Pascal <pascal.knecht@hotmail.ch>
2020-04-05 11:33:35 +0200

    Section 111 - conditional displaying

--- src/App.js
+++ src/App.js
@@ -40,11 +40,17 @@ const App = () => {
   const [searchTerm, setSearchTerm] = useSemiPersistentState("search", "Re");
 
   const [stories, setStories] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [isError, setIsError] = React.useState(false);
 
   React.useEffect(() => {
-    getAsyncStories().then((result) => {
-      setStories(result.data.stories);
-    });
+    setIsLoading(true);
+    getAsyncStories()
+      .then((result) => {
+        setStories(result.data.stories);
+        setIsLoading(false);
+      })
+      .catch(() => setIsError(true));
   }, []);
 
   const handleRemoveStory = (item) => {
@@ -78,7 +84,13 @@ const App = () => {
 
       <hr />
 
-      <List list={searchedStories} onRemoveItem={handleRemoveStory} />
+      {isError && <p>Something went wrong</p>}
+
+      {isLoading ? (
+        <p>Loading ...</p>
+      ) : (
+        <List list={searchedStories} onRemoveItem={handleRemoveStory} />
+      )}
     </div>
   );
 };
